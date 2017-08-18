95942cd8bdf87d9fd92cd5a60aec03f3bb917d1a
Evgeniy Feder <dzaba67@yandex.ru>
2017-08-18 12:55:36 +0300

    Ended sudoku cracker

--- sudoku/data.py
+++ sudoku/data.py
@@ -3,9 +3,10 @@ from math import sqrt
 
 class data:
     def __init__(self, filename):
-        self.n = len(self.digits)
         self.digits = [i for i in open(filename).read() if i in '123456789.']
-        self.group()
+        self.n = int(sqrt(len(self.digits)))
+        self.digits = self.group()
+        self.max_number = self.n
 
     # Read from file module
     def group(self):
@@ -21,27 +22,25 @@ class data:
     def get_row(self, pos):
         res = []
         for i in range(self.n):
-            res.append(self.digits[pos[0]][i])
+            res.append(self.digits[pos[1]][i])
         return res
 
     def get_col(self, pos):
         res = []
         for i in range(self.n):
-            res.append(self.digits[i][pos[1]])
+            res.append(self.digits[i][pos[0]])
         return res
 
     def get_block(self, pos):
         res = []
         block_size = int(sqrt(self.n))
 
-        begin_x = (pos[0] / block_size) * block_size
-        begin_y = (pos[1] / block_size) * block_size
-        end_x = (pos[0] / block_size + 1) * block_size
-        end_y = (pos[1] / block_size + 1) * block_size
+        begin_x = (pos[0] // block_size) * block_size
+        begin_y = (pos[1] // block_size) * block_size
+        end_x = (pos[0] // block_size + 1) * block_size
+        end_y = (pos[1] // block_size + 1) * block_size
 
         for y in range(begin_y, end_y):
-            row = []
             for x in range(begin_x, end_x):
-                row.append(self.digits[y][x])
-            res.append(row)
+                res.append(self.digits[y][x])
         return res
--- sudoku/game.py
+++ sudoku/game.py
@@ -1,4 +1,56 @@
 import data
+from pprint import pprint
+
 
 class game:
-    def __init__(self):
+    def __init__(self, input_file):
+        self.grid = data.data(input_file)
+        pprint(self.grid.digits)
+
+    def find_next_empty(self, begin_coord):
+        exit_flag = False
+        res = (-1, -1)
+        for y in range(begin_coord[1], self.grid.n):
+            if y == begin_coord[1]:
+                begin_x = begin_coord[0]
+            else:
+                begin_x = 0
+
+            for x in range(begin_x, self.grid.n):
+                if self.grid.digits[y][x] == '.':
+                    res = (x, y)
+                    exit_flag = True
+                    break
+            if exit_flag:
+                break
+        return res
+
+    def get_existed_numbers(self, cur_coord):
+        row, col, block = (self.grid.get_row(cur_coord),
+                           self.grid.get_col(cur_coord),
+                           self.grid.get_block(cur_coord))
+
+        res = []
+        for i in range(1, self.grid.max_number + 1):
+            cur = str(i)
+            if not (cur in row or cur in col or cur in block):
+                res.append(cur)
+        return res
+
+    def solve(self, cur_coord):
+        next = self.find_next_empty(cur_coord)
+
+        if next == (-1, -1):
+            pprint(self.grid.digits)
+            return
+
+        can_num = self.get_existed_numbers(next)
+
+        for i in can_num:
+            self.grid.digits[next[1]][next[0]] = str(i)
+            self.solve(next)
+            self.grid.digits[next[1]][next[0]] = '.'
+
+g = game("a.txt")
+g.solve((0,0))
+
